297fe285940ff10a221d4048e840d614a9d6c3c7
Kenneth V. Domingo <hello@kvd.studio>
2026-01-08 23:40:52 +0800

    ci: create time machine user

--- main.tf
+++ main.tf
@@ -21,17 +21,34 @@ provider "postgresql" {
   sslmode  = "disable"
 }
 
+locals {
+  application_users = tomap({
+    inf_test = {
+      name = "inf_test"
+      password = data.infisical_secrets.default.secrets["INF_TEST_USER_PASSWORD"].value
+    }
+    time_machine = {
+      name = "time_machine"
+      password = data.infisical_secrets.default.secrets["DB_PASSWORD_TIME_MACHINE"].value
+    }
+  })
+}
+
 resource "postgresql_role" "inf_test" {
-  name                      = "inf_test_user"
+  for_each = local.application_users
+
+  name                      = each.value["name"]
   replication               = false
   login                     = true
-  password_wo               = data.infisical_secrets.default.secrets["INF_TEST_USER_PASSWORD"].value
+  password_wo               = each.value["password"]
   password_wo_version       = "1"
   bypass_row_level_security = false
 }
 
 resource "postgresql_database" "inf_test" {
-  name                   = "inf_test"
-  owner                  = postgresql_role.inf_test.name
+  for_each = local.application_users
+
+  name                   = each.value["name"]
+  owner                  = postgresql_role.inf_test[each.key].name
   alter_object_ownership = true
 }
